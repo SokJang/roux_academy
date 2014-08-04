f5838093a5ca126b3e7401a939916d8d93eccf96
Sok <s.jang@sokdesign.de>
2014-08-04 18:49:57 +0200

    Added skeleton for mainpages. index.php, about.php, artists.php, privacypolicy.php, register.php, schedule.php, venuetravel.php

--- artists.php
+++ artists.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Roux Academy Conference 2014 -- Home</title>
+    <title>Roux Academy Conference 2014 -- Artists</title>
     <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -14,7 +14,7 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body id="home">
+  <body id="artists">
     
     <section class="container">
       <div class="content row">
