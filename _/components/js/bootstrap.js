70e17f15128111ce6d6eb04d6fedfcc255fa1748
Sok <s.jang@sokdesign.de>
2014-08-04 15:20:04 +0200

    bugfix on bootstrap.js reordering of appending js-files in correct order.

--- _/components/js/bootstrap.js
+++ _/components/js/bootstrap.js
@@ -24,16 +24,17 @@
  * Date: 2014-05-01T17:42Z
  */
 
- //@prepros-append jquery.js
- //@prepros-append affix.js
- //@prepros-append alert.js
- //@prepros-append button.js
- //@prepros-append carousel.js
- //@prepros-append collapse.js
- //@prepros-append dropdown.js
- //@prepros-append modal.js
- //@prepros-append popover.js
- //@prepros-append scrollspy.js
- //@prepros-append tab.js
- //@prepros-append transition.js
- //@prepros-append myjs.js
+// @prepros-append "jquery.js"
+// @prepros-append "affix.js"
+// @prepros-append "transition.js"
+// @prepros-append "tooltip.js"
+// @prepros-append "alert.js"
+// @prepros-append "button.js"
+// @prepros-append "carousel.js"
+// @prepros-append "collapse.js"
+// @prepros-append "dropdown.js"
+// @prepros-append "modal.js"
+// @prepros-append "popover.js"
+// @prepros-append "scrollspy.js"
+// @prepros-append "tab.js"
+// @prepros-append "myjs.js"
--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Roux Academy Conference 2014 -- Home</title>
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -13,9 +14,19 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
+  <body id="home">
     
-    <h1>Hello, world!</h1>
+    <header>
+      <nav></nav>
+    </header>
+    
+    <section class="container">
+
+      <article></article>
+      <aside></aside>
+    </section><!-- container -->
+    
+    <footer></footer>
     
     
     <script src="_/js/bootstrap.js"></script>
