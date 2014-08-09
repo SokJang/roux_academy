db60d20f4bcb49b84e9f01cd3fd8c32f3091708d
Sok <s.jang@sokdesign.de>
2014-08-09 13:27:45 +0200

    about.php, artists.php, privacypolicy.php, register.php, schedule.php, venuetravel.php got header.php included

--- _/components/js/myjs.js
+++ _/components/js/myjs.js
@@ -1,6 +1,8 @@
 $(function() {
 
 	//highlight the current nav
+	//Selects the elemtent: dependend on the page id (Home) and dependend on the content eg 'Home' between the a tag
+								// adds class 'active' to the parent element which is li tag
 	$("#home a:contains('Home')").parent().addClass('active');
 	$("#schedule a:contains('Schedule')").parent().addClass('active');
 	$("#artists a:contains('Artists')").parent().addClass('active');
@@ -9,6 +11,8 @@ $(function() {
 
 
 	//make menus drop automatically
+	//Selects ul element with class nav and selcts from there the li element with the class dropdown
+						// on hover the function triggers
 	$('ul.nav li.dropdown').hover(function() {
 		$('.dropdown-menu', this).fadeIn();
 	}, function() {
