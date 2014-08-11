bb0750a634355c062f8148d006fc214dd3b7e057
Sok <s.jang@sokdesign.de>
2014-08-11 10:54:10 +0200

    header.php includes put in row container for all php files except footer.php. added all carousel images in snippet-carousel.php

---
+++ _/components/php/snippet-carousel.php
@@ -0,0 +1,19 @@
+<div id="myCarousel">
+	<section class="carousel-inner">
+		<div class="item">
+			<img src="images/carousel/seattle_skyline.jpg" alt="Photo of Seattle Skyline">
+		</div>
+		<div class="item">
+			<img src="images/carousel/jewelry_workshop.jpg" alt="Photo of Jewelry Workshop">
+		</div>
+		<div class="item">
+			<img src="images/carousel/painting_workshop.jpg" alt="Photo of Painting Workshop">
+		</div>
+		<div class="item">
+			<img src="images/carousel/pottery_workshop.jpg" alt="Photo of Pottery Workshop">
+		</div>
+		<div class="item">
+			<img src="images/carousel/quicksketch_workshop.jpg" alt="Photo of Quick Sketch Workshop">
+		</div>
+	</section><!-- carousel inner -->
+</div><!-- myCarousel -->
