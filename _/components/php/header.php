b2ae08b8849dca70482b50fb1c8f0fdf42272812
Sok <s.jang@sokdesign.de>
2014-08-12 17:17:06 +0200

    riddding visual bug by deleting 2 divs around header tag in header.php

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,5 +1,3 @@
-<div class="content row">
-	<div class="col-lg-12">
 		<header class="clearfix">
 			<section id="branding">
 				<a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="Logo for Roux Conference"></a>
@@ -41,5 +39,3 @@
 			</section><!-- modal -->
 
 		</header><!-- header -->
-	</div><!-- column -->
-</div><!-- content -->
