85c962ec585f8aa35612d17387796cb652b44cc3
Sok <s.jang@sokdesign.de>
2014-08-12 11:18:22 +0200

    inserted data-spy and data-target in body tag of artists.php ot enable scrollspy

--- artists.php
+++ artists.php
@@ -7,7 +7,7 @@
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
-  <body id="artists">
+  <body id="artists" data-spy="scroll" data-target=".scrollspy"><!-- data-target targets the section - in article-artistlist.php - with the class of scrollsyp -->
 
 		<section class="container">
       <div class="content row">
